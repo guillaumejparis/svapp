8c421fc00d97134d509b2d8b2af0e5b129f3db67
Guillaume <guillaumejparis@gmail.com>
2021-05-02 17:26:28 +0200

    Big update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,58 +1,154 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 300px;
-	}
-
-	.image-logo {
-		max-width: 100px;
-	}
-
-	.logos {
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
-	}
-
-	p {
-		margin: 0 auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Webapp</title>
+  <title>Jean Maurice</title>
 </svelte:head>
 
-<h1>Hello home!</h1>
+<h1>
+  {[
+    "Hey",
+    "Hello",
+    "Hi",
+    "Howdy",
+    "Bonjour",
+    "Good day",
+    "Aloha",
+    "Yo",
+    "Namaste",
+    "Howdy-do",
+    "Cheerio",
+    "G'day",
+    "Good day",
+    "Sup",
+    "Salute",
+  ][Math.floor(Math.random() * 15)]} !
+</h1>
 
 <figure>
-	<div class="logos">
-		<a href="/domoticz">
-			<img class="image-logo" src="https://upload.wikimedia.org/wikipedia/commons/8/82/Domoticz.png">
-		</a>
-		<a href="/bw">
-			<img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Bitwarden_Logo_Horizontal.png">
-		</a>
-		<a href="https://guillaumejparis.github.io/recipes">
-			<img class="image-logo" src="https://upload.wikimedia.org/wikipedia/commons/2/2c/GNOME_Recipes_logo.png">
-		</a>
-	</div>
+  <div class="container">
+    <div class="logos-couple">
+      <a href="/domoticz" class="logo-left">
+        <img
+          alt="domoticz"
+          src="http://domoticx.com/wp-content/uploads/2018/01/domoticz-logo.png"
+        />
+      </a>
+      <a href="/bw" class="logo-right">
+        <img
+          alt="bitwarden"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Bitwarden_Logo_Horizontal.png"
+        />
+      </a>
+    </div>
+    <div class="logos-couple">
+      <a href="https://guillaumejparis.github.io/recipes" class="logo-left">
+        <img
+          alt="recipes"
+          src="https://iconape.com/wp-content/files/aq/167156/svg/bon-appetit-seeklogo.com.svg"
+        />
+      </a>
+      <a href="/qb" class="logo-right">
+        <img
+          alt="qbittorrent"
+          src="https://upload.wikimedia.org/wikipedia/commons/3/39/BitTorrent_logo.svg"
+        />
+      </a>
+    </div>
+    <div class="logos-couple">
+      <a href="/netdata" class="logo-left">
+        <img alt="netdata" src="https://landscape.cncf.io/logos/netdata.svg" />
+      </a>
+      <a href="/plex" class="plex">
+        <img
+          id="plex-logo-img"
+          alt="plex"
+          src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Plex_favicon.png"
+        />
+        <img
+          id="plex-logo-text"
+          alt="plex"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Plex_vector_logo.svg/2880px-Plex_vector_logo.svg.png"
+        />
+      </a>
+    </div>
+  </div>
 </figure>
+
+<style>
+  h1,
+  figure {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  img {
+    max-height: 77px;
+    max-width: 100%;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 40px;
+    max-width: 100%;
+  }
+
+  .logos-couple {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    width: 100%;
+  }
+
+  .logo-left {
+    display: flex;
+    margin-right: 25px;
+    height: 100%;
+    flex: 1;
+    justify-content: flex-end;
+  }
+
+  .logo-right {
+    display: flex;
+    margin-left: 25px;
+    height: 100%;
+    flex: 1;
+    justify-self: flex-start;
+  }
+
+  .plex {
+    display: flex;
+    margin-left: 25px;
+    flex: 1;
+    justify-self: flex-start;
+    align-items: center;
+  }
+
+  #plex-logo-img {
+    max-height: 62px;
+    max-width: 30%;
+  }
+
+  #plex-logo-text {
+    max-height: 62px;
+    max-width: calc(70% - 10px);
+    margin-left: 10px;
+  }
+
+  @media screen and (min-width: 480px) {
+    h1 {
+      font-size: 6em;
+    }
+    #plex-logo-text {
+      max-width: calc(70% - 30px);
+      margin-left: 30px;
+    }
+  }
+</style>
