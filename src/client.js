b5cb8c47576f0e6bf3b0f32501041cd52d0a9041
Guillaume Paris <guillaumejparis@gmail.com>
2021-05-27 11:24:04 +0200

    Open qb & go back to previous page

--- src/client.js
+++ src/client.js
@@ -1,7 +1,8 @@
 import * as sapper from '@sapper/app';
 
 if (window.location.hash && window.location.hash.includes("download")) {
-	window.location = "/qb" + window.location.hash;
+	window.open("/qb" + window.location.hash, "_blank");
+	history.back();
 }
 
 sapper.start({
